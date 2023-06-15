92b204b4e51d7495a309561283ca6fae4f997963
eugenepentland <eugenepentland@gmail.com>
2023-06-15 15:03:08 -0400

    updated project structure and added contribution.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/UnderstandGPT/UnderstandGPT',
             label: 'GitHub',
